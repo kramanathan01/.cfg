1f8ca03ee4be224437d155615db97c0f1df95751
kramanathan01 <5899848+kramanathan01@users.noreply.github.com>
2022-10-11 22:54:24 -0700

    Add more aliases
    Signed-off-by: kramanathan01 <5899848+kramanathan01@users.noreply.github.com>

--- .config/zsh/aliases.zsh
+++ .config/zsh/aliases.zsh
@@ -48,5 +48,10 @@ else
 fi
 
 # Jagannatha Hora alias
+alias jd="cd ~/.wine/drive_c/Program\ Files\ \(x86\)/Jagannatha\ Hora/data"
+alias jh="wine64 ~/.wine/drive_c/Program\ Files\ \(x86\)/Jagannatha\ Hora/bin/jhora.exe&"
 
+# System aliases
 alias duf="du -sh * | sort -hr"
+alias airport="/System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources/airport"
+alias afk="open -a /System/Library/CoreServices/ScreenSaverEngine.app"
